9c77001130472b0c3ac5c5ff8b1f57876feeef2e
SongJiHyung <gud4808@gmail.com>
2023-05-15 09:33:02 +0900

    5/15 기본 패키지 작성
    기본 패키지 작성  했습니다

--- artpart/src/main/generated/org/ict/artpart/member/entity/QMemberEntity.java
+++ artpart/src/main/generated/org/ict/artpart/member/entity/QMemberEntity.java
@@ -7,6 +7,7 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -17,9 +18,11 @@ public class QMemberEntity extends EntityPathBase<MemberEntity> {
 
     private static final long serialVersionUID = -1692809142L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QMemberEntity memberEntity = new QMemberEntity("memberEntity");
 
-    public final StringPath memberAdd = createString("memberAdd");
+    public final org.ict.artpart.apt.entity.QAptEntity aptIdx;
 
     public final StringPath memberDong = createString("memberDong");
 
@@ -31,22 +34,35 @@ public class QMemberEntity extends EntityPathBase<MemberEntity> {
 
     public final StringPath memberLoginOK = createString("memberLoginOK");
 
+    public final StringPath memberMyCar1 = createString("memberMyCar1");
+
+    public final StringPath memberMyCar2 = createString("memberMyCar2");
+
     public final StringPath memberName = createString("memberName");
+
+    public final StringPath memberPassword = createString("memberPassword");
 
     public final StringPath memberPhone = createString("memberPhone");
 
-    public final StringPath memberPw = createString("memberPw");
-
     public QMemberEntity(String variable) {
-        super(MemberEntity.class, forVariable(variable));
+        this(MemberEntity.class, forVariable(variable), INITS);
     }
 
     public QMemberEntity(Path<? extends MemberEntity> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QMemberEntity(PathMetadata metadata) {
-        super(MemberEntity.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QMemberEntity(PathMetadata metadata, PathInits inits) {
+        this(MemberEntity.class, metadata, inits);
+    }
+
+    public QMemberEntity(Class<? extends MemberEntity> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.aptIdx = inits.isInitialized("aptIdx") ? new org.ict.artpart.apt.entity.QAptEntity(forProperty("aptIdx")) : null;
     }
 
 }
--- artpart/src/main/java/org/ict/artpart/member/controller/MemberController.java
+++ artpart/src/main/java/org/ict/artpart/member/controller/MemberController.java
@@ -4,7 +4,7 @@ package org.ict.artpart.member.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ict.artpart.common.Header;
-import org.ict.artpart.member.model.dto.Member;
+import org.ict.artpart.member.model.dto.MemberDto;
 import org.ict.artpart.member.model.service.MemberService;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +17,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/login/{memberid}")
-    public Header<Member> login(@PathVariable String memberid, @RequestParam("memberpwd") String memberpwd){
-        Member member = memberService.login(memberid, memberpwd);
+    public Header<MemberDto> login(@PathVariable String memberid, @RequestParam("memberpwd") String memberpwd){
+        System.out.println(memberid);
+        MemberDto member = memberService.login(memberid, memberpwd);
         if(member != null){
             return Header.OK(member);
         } else {
