768f21bb5461c54529e2dc3967fc422f2afbd517
okjung123 <ttwmrk2002@naver.com>
2023-05-19 14:33:46 +0900

    언더바 수정

--- artpart/src/main/java/org/ict/artpart/member/entity/MemberEntity.java
+++ artpart/src/main/java/org/ict/artpart/member/entity/MemberEntity.java
@@ -26,47 +26,47 @@ public class MemberEntity {
 
     @ManyToOne
     @Comment("아파트 번호")
-    @JoinColumn(name = "APT_IDX")
+    @JoinColumn(name = "APTIDX")
     private AptEntity aptIdx;
 
     @Comment("동")
-    @Column(name="MEMBER_DONG")
+    @Column(name="MEMBERDONG")
     private String memberDong;
 
     @Comment("호수")
-    @Column(name="MEMBER_HO")
+    @Column(name="MEMBERHO")
     private String memberHo;
 
     @Comment("이름")
-    @Column(name = "MEMBER_NAME")
+    @Column(name = "MEMBERNAME")
     private String memberName;
 
     @Comment("아이디")
-    @Column(name = "MEMBER_ID")
+    @Column(name = "MEMBERID")
     private String memberId;
 
     @Comment("비밀번호")
-    @Column(name = "MEMBER_PASSWORD")
+    @Column(name = "MEMBERPASSWORD")
     private String memberPassword;
 
     @Comment("전화번호")
-    @Column(name = "MEMBER_PHONE")
+    @Column(name = "MEMBERPHONE")
     private String memberPhone;
 
     @Comment("입주완료여부")
-    @Column(name = "MEMBER_LOGINOK")
+    @Column(name = "MEMBERLOGINOK")
     private String memberLoginOK;        //입주완료여부
 
 
     @Comment("등록차 번호1")
-    @Column(name = "MEMBER_MYCAR1")
+    @Column(name = "MEMBERMYCAR1")
     private String memberMyCar1;
 
     @Comment("등록차 번호2")
-    @Column(name = "MEMBER_MYCAR2")
+    @Column(name = "MEMBERMYCAR2")
     private String memberMyCar2;         //등록차 번호2
 
-    @Column(name = "MEMBER_AUTH")
+    @Column(name = "MEMBERAUTH")
     private String memberAuth;
 
 }
