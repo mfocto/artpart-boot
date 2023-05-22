61876ef18922876d29e0292286257a6ab6a91630
nakhyun <skrgus0140@naver.com>
2023-05-22 14:30:30 +0900

    전표, 현금출납부 기능 완성!!!

--- artpartM/src/main/java/org/ict/artpartM/account/entity/SlipRepository.java
+++ artpartM/src/main/java/org/ict/artpartM/account/entity/SlipRepository.java
@@ -2,5 +2,8 @@ package org.ict.artpartM.account.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SlipRepository extends JpaRepository<SlipEntity, Long> {
+    SlipEntity findBySlipIdx(long slipIdx);
 }
